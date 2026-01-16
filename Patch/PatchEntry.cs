06e1aa1411692051a2d0cf9d0fecb8ead228e102
LaCumbiaDelCoronavirus <90893484+LaCumbiaDelCoronavirus@users.noreply.github.com>
2026-01-17 00:15:28 +0800

    better

--- Patch/PatchEntry.cs
+++ Patch/PatchEntry.cs
@@ -1,18 +1,17 @@
 using System.Reflection;
 using HarmonyLib;
 
-
 #pragma warning disable CA1050 // Declare types in namespace
-// Do not change signature or name or namespace
+// Do not change signature or name or namespace; this is found via reflection
 public static class PatchEntry
 #pragma warning restore CA1050 // Declare types in namespace
 {
-    // Do not change signature or name or namespace
+    // Do not change signature or name or namespace; this is found via reflection
     public static void Entry(Dictionary<string, Assembly?> assemblyDictionary)
     {
         if (!assemblyDictionary.TryGetValue("Content.Client", out _))
             return;
 
-        new Harmony("sanabi.ext.dpatch").PatchAll(Assembly.GetExecutingAssembly());
+        new Harmony($"sanabi.ext.{new Guid()}").PatchAll(Assembly.GetExecutingAssembly());
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Template patch for SanabiLauncher.
-Comes with .DLLs used by the patch to recognise types in the game assemblies.
+Comes with .DLLs (Windows, x64) used by patch code to recognise types in the game assemblies.
